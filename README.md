c73d0d70b169d82ea8b0407d4dae08be87c75fec
gurasan0110 <gurasan0110@icloud.com>
2025-01-26 09:22:09 +0900

    Create project

---
+++ README.md
@@ -0,0 +1,3 @@
+# flutter_app_icon_practice
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/gurasan0110/flutter_app_icon_practice/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.gurasan0110.flutter_app_icon_practice
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
